3071aa573f05fbec94e2b19a71a93c64a617f3c2
vijay9711 <vijaysm97@gmail.com>
2024-12-26 23:01:05 -0600

    removed tailwind

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // Function to load content from an external HTML file into a target element
-  const loadContent = (file, targetId) => {
-    fetch(file)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`Failed to load ${file}`);
-        }
-        return response.text();
-      })
-      .then(html => {
-        const target = document.querySelector(`#${targetId}`);
-        if (target) {
-          target.innerHTML = html;
-        } else {
-          console.error(`Target #${targetId} not found`);
-        }
-      })
-      .catch(error => console.error(error));
-  };
-
-  // Load the content for each section
-  loadContent('./src/home/home.html', 'home');
-  loadContent('./src/about/about.html', 'about');
+  
 });
